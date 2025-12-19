bd206db768656e1749b32ecd8d84be5381d9f4f7
Domar <omerserifoduncu@gmail.com>
2025-12-19 09:33:45 +0300

    pencereden çıkan ışık

--- Scripts/InteriorAutoLights.gd
+++ Scripts/InteriorAutoLights.gd
@@ -1,23 +1,31 @@
-extends TileMap
+extends Node2D
 
-var windows = get_used_cells_by_id(0,0,Vector2(1,0))
-var source:TileSetAtlasSource = tile_set.get_source(0) as TileSetAtlasSource
-var rect = source.get_tile_texture_region(Vector2(1,0))
-var image:Image = source.get_texture().get_image()
-var tile_image:Image = image.get_region(rect)
+@onready var tilemap = get_parent()
+@export var LIGHT_LENGTH = 32
+var windows
+#var source:TileSetAtlasSource = tile_set.get_source(0) as TileSetAtlasSource
+#var rect = source.get_tile_texture_region(Vector2(1,0))
+#var image:Image = source.get_texture().get_image()
+#var tile_image:Image = image.get_region(rect)
+var tex:ImageTexture
 func _ready() -> void:
-	for i in tile_image.get_height():
-		for i2 in tile_image.get_width():
-			if tile_image.get_pixel(i2,i) == Color.TRANSPARENT:
-				tile_image.set_pixel(i2,i,Color.WHITE)
-			else:
-				tile_image.set_pixel(i2,i,Color.TRANSPARENT)
+	windows = tilemap.get_used_cells_by_id(0,0,Vector2(1,0))
+	#for i in tile_image.get_height():
+	#	for i2 in tile_image.get_width():
+	#		if tile_image.get_pixel(i2,i) == Color.TRANSPARENT:
+	#			tile_image.set_pixel(i2,i,Color.WHITE)
+	#		else:
+	#			tile_image.set_pixel(i2,i,Color.TRANSPARENT)
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	queue_redraw()
 
 func _draw() -> void:
+	var start = 16
+	tex = ImageTexture.create_from_image(Image.load_from_file("res://Sprites/Light.png"))
 	for vec in windows:
-		for i in range(-8,8):
-			var worldPos = (map_to_local(vec)) + Vector2(-8,-8)
-			draw_texture(ImageTexture.create_from_image(tile_image),worldPos)
+		for i in range(-start,LIGHT_LENGTH):
+			tex.set_size_override(Vector2(13+(i+start)/2,22))
+			var worldPos = tilemap.map_to_local(vec) + Vector2(-8-(i+start)/4,i)
+			draw_texture(tex,worldPos,Color(1,1,1,0.1))
