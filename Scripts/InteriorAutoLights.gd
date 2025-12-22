f8ceb78c945b6d745a221e14585a69be2139dff5
Domar <omerserifoduncu@gmail.com>
2025-12-22 21:14:39 +0300

    sonunda ışık işi tamamen halloldu

--- Scripts/InteriorAutoLights.gd
+++ Scripts/InteriorAutoLights.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-@onready var lightArea = get_parent().get_child(1)
+var lightArea
+var lightCollider
+var lightColliderShape = ConvexPolygonShape2D.new()
 @onready var tilemap = get_parent()
 @export var LIGHT_LENGTH = 32
 var windows
@@ -10,7 +12,6 @@ var windows
 #var tile_image:Image = image.get_region(rect)
 var tex:ImageTexture
 func _ready() -> void:
-	lightArea.add_child(tilemap.get_node(^"CollisionShape2D"))
 	windows = tilemap.get_used_cells_by_id(0,0,Vector2(1,0))
 	#for i in tile_image.get_height():
 	#	for i2 in tile_image.get_width():
@@ -28,6 +29,13 @@ func _draw() -> void:
 	var start = 16
 	tex = ImageTexture.create_from_image(Image.load_from_file("res://Sprites/Light.png"))
 	for vec:Vector2 in windows:
+		lightArea = load("res://Scenes/LightArea.tscn").instantiate()
+		lightCollider = CollisionShape2D.new()
+		lightColliderShape.set_point_cloud(PackedVector2Array([Vector2(5,0),Vector2(10,0),Vector2(20,48),Vector2(-5,48)]))
+		lightCollider.set_shape(lightColliderShape)
+		lightArea.add_child(lightCollider)
+		lightArea.position = tilemap.map_to_local(vec) + Vector2(-8,-8)
+		tilemap.add_child(lightArea)
 		for i in range(-start,LIGHT_LENGTH):
 			tex.set_size_override(Vector2(13+(i+start)/2,22))
 			var worldPos = tilemap.map_to_local(vec) + Vector2(-8-(i+start)/4,i)
--- Scripts/LightArea.gd
+++ Scripts/LightArea.gd
@@ -5,28 +5,36 @@ var lightTimer = 0.0
 @onready var player = $/root/Game/Modulate/Player
 @onready var moon = $/root/Game/Camera2D/Moon
 @onready var sun = $/root/Game/Camera2D/Sun
-var collisionset = false
+
+func _ready() -> void:
+	set_collision_layer_value(get_parent().get_tileset().get_physics_layer_collision_mask(0),true)
+	set_collision_mask_value(get_parent().get_tileset().get_physics_layer_collision_mask(0),true)
 
 func _physics_process(delta: float) -> void:
-	if get_node_or_null(^"CollisionShape2D") and not collisionset:
-		set_collision_layer_value(get_parent().get_tileset().get_physics_layer_collision_mask(0),true)
-		set_collision_mask_value(get_parent().get_tileset().get_physics_layer_collision_mask(0),true)
-		collisionset = true
+	#$Label.text = str(isPlayerInLight and not player.isInNoLightArea)
 	if get_collision_mask_value(get_parent().get_tileset().get_physics_layer_collision_mask(0)) != player.get_collision_layer_value(get_parent().get_tileset().get_physics_layer_collision_mask(0)):
 		isPlayerInLight = false
 		lightTimer = 0.0
-	#$/root/Game/Label.text = "Light Timer: {0}, Is Player In: {1}".format([str(lightTimer),str(isPlayerIn)])
+	#$Label.text = "Light Timer: {0}".format([str(lightTimer)])
 	if (not player.isInNoLightArea) and isPlayerInLight and player.HEALTH > 0 and sun.position.y < moon.position.y:
 		lightTimer -= delta
 		if lightTimer <= 0:
 			player.HEALTH -= 1
 			player.knockback(0,3.5)
-			lightTimer = 120.0/-sun.position.y
+			lightTimer = lerp(6,1,-sun.position.y/60)
+			if lightTimer > 6:
+				lightTimer = 6
+			elif lightTimer < 1:
+				lightTimer = 1
 
 func _on_body_entered(body: Node2D) -> void:
 	if body == player:
 		isPlayerInLight = true
-		lightTimer = 120.0/-sun.position.y
+		lightTimer = lerp(6,1,-sun.position.y/60)
+		if lightTimer > 6:
+			lightTimer = 6
+		elif lightTimer < 1:
+			lightTimer = 1
 func _on_body_exited(body: Node2D) -> void:
 	if body == player:
 		isPlayerInLight = false
